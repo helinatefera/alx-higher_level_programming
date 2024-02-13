092b264bd32211f9ccff6118548ec1f19bd8c208
helinatefera <helinatefera1212@gmail.com>
2024-02-13 12:01:24 -0800

    9

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
--- Insert values
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- Count rows with id 89
+-- Execute: cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
+SELECT COUNT(*) FROM first_table WHERE id=89;
