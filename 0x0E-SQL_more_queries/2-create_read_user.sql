279edc53f9836bccd1e06be0848cc494b3a063fd
helinatefera <helinatefera1212@gmail.com>
2024-02-14 11:19:39 -0800

    Done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -9,10 +9,4 @@ GRANT SELECT
    ON `hbtn_0d_2`.*
    TO 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;-- Creates the table hbtn_0d_usa with table states.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`),
-    `id`   INT          NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
-);
+FLUSH PRIVILEGES;
