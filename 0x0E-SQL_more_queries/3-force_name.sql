dbfba813945fa1b61e75fc885406943e7a5154bf
helinatefera <helinatefera1212@gmail.com>
2024-02-14 10:52:09 -0800

    7

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,4 +2,16 @@
 CREATE TABLE IF NOT EXISTS `force_name` (
     `id`   INT,
     `name` VARCHAR(256) NOT NULL
+);-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
